086f158d61dd924c8cab1064bb3d2ba4b9b64390
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2018-12-23 21:42:13 +0100

    initial commit

---
+++ LegalNotice/Public/Set-LegalNotice.ps1
@@ -0,0 +1,48 @@
+﻿function Set-LegalNotice {
+    [CmdletBinding(DefaultParameterSetName='Online')]
+    param (
+        # Caption/Title for the legal notice
+        [Parameter(Mandatory)]
+        [string]
+        $Caption,
+        # Text for the legal notice
+        [Parameter(Mandatory)]
+        [string]
+        $Text,
+        # Computers to apply legal otice on
+        [Parameter(ParameterSetName='Online')]
+        [string[]]
+        $ComputerName = @($env:COMPUTERNAME),
+        # Desired path for legal notice reg-file for offline usage
+        [Parameter(ParameterSetName='File')]
+        [string]
+        $Path = ".\LegalNotice.reg",
+    )
+
+    begin {
+        if ($psCmdlet.ParameterSetName -eq 'File') {
+            $hexText = "hex(1):$(([System.Text.Encoding]::Unicode.GetBytes($Text) | % { "{0:X2}" -f $_ }) -join ','),00,00"
+        } else {
+            Write-Error "Online-functionality not yet implemented. Use Path-parameter to generate registry-file for import." -Category NotImplemented
+        }
+    }
+
+    process {
+        if ($psCmdlet.ParameterSetName -eq 'File') {
+            $content = @"
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon]
+"LegalNoticeCaption"="$($Caption.Trim())"
+"LegalNoticeText"=$hex
+"@
+            Set-Content -Path $Path -Value $content
+            Write-Host "Legal notice registry-file saved to:"
+            Resolve-Path -Path $Path | Select-Object -ExpandProperty Path
+        }
+
+    }
+
+    end {
+    }
+}
